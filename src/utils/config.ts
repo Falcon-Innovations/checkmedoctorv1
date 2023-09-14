22e86eae314a13e8dc3f524f22c18df714423bb8
eldrige <peldriige8@gmail.com>
2023-09-14 22:32:58 +0100

    api: update api url

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,5 @@
-export const BASE_URL = 'http://localhost:8080/api/v1';
+export const BASE_URL =
+  'https://check-me-backend-cameroun.herokuapp.com/api/v1';
 
 export const X_CSCAPI_KEY =
   'bG52M0FQbUpQQk9HYjdoRzBtR0ZNeXE0anAyWGFjbFZaS051WXpWdQ==';
